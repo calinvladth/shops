feab11d59b8a5c5ff4304155fbbb09af1fa72e3e
Calin Vlad <calinvlad@Calins-MacBook-Pro.local>
2025-09-22 21:55:12 +0300

    shop details

--- app/models/shops.py
+++ app/models/shops.py
@@ -5,8 +5,22 @@ class ShopModel(db.Model):
     __tablename__ = "shops"
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120), nullable=False)
+    name = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.Text, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+
+    latitude = db.Column(db.Text, nullable=False)
+    longitude = db.Column(db.Text, nullable=False)
+
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
 
     def to_dict(self):
-        return {"id": self.id, "title": self.title, "user_id": self.user_id}
+        return {
+            "id": self.id,
+            "name": self.name,
+            "user_id": self.user_id,
+            "address": self.address,
+            "city": self.city,
+            "latitude": self.latitude,
+            "longitude": self.longitude,
+        }
--- app/resources/shops.py
+++ app/resources/shops.py
@@ -6,50 +6,118 @@ from wrappers import shop_required
 
 class ShopList(Resource):
     def get(self):
-        shops = ShopModel.query.all()
+        try:
+            shops = ShopModel.query.all()
 
-        return [shop.to_dict() for shop in shops]
+            return [shop.to_dict() for shop in shops]
+
+        except Exception as e:
+            return f"something went wrong: {e}", 500
 
     @shop_required()
     def post(self, user):
-        data = request.get_json()
-        new_shop = ShopModel(title=data["title"], user_id=user.id)
-        db.session.add(new_shop)
-        db.session.commit()
+        try:
+            data = request.get_json()
+            name = data.get("name", "").strip()
+            address = data.get("address", "").strip()
+            city = data.get("city", "").strip()
+            latitude = data.get("latitude", 0)
+            longitude = data.get("longitude", 0)
 
-        return new_shop.to_dict(), 201
+            if not name or len(name) == 0:
+                return "name is missing", 500
+
+            if not address or len(address) == 0:
+                return "address is missing", 500
+
+            if not city or len(city) == 0:
+                return "city is missing", 500
+
+            if not latitude:
+                return "latitude is missing", 500
+
+            if not longitude:
+                return "longitude is missing", 500
+
+            new_shop = ShopModel(
+                name=name,
+                address=address,
+                city=city,
+                latitude=latitude,
+                longitude=longitude,
+                user_id=user.id,
+            )
+            db.session.add(new_shop)
+            db.session.commit()
+
+            return new_shop.to_dict(), 201
+
+        except Exception as e:
+            return f"something went wrong: {e}", 500
 
 
 class ShopResource(Resource):
     def get(self, shop_id):
-        shop = ShopModel.query.get(shop_id)
+        try:
+            shop = ShopModel.query.get(shop_id)
 
-        if not shop:
-            return "shop not found", 404
+            if not shop:
+                return "shop not found", 404
 
-        return shop.to_dict()
+            return shop.to_dict()
+
+        except Exception as e:
+            return f"something went wrong: {e}", 500
 
     @shop_required()
     def put(self, shop_id, user):
-        data = request.get_json()
-        shop = ShopModel.query.filter_by(id=shop_id, user_id=user.id).first()
+        try:
+            data = request.get_json()
+            name = data.get("name", "").strip()
+            address = data.get("address", "").strip()
+            city = data.get("city", "").strip()
+            latitude = data.get("latitude", 0)
+            longitude = data.get("longitude", 0)
 
-        if not shop:
-            return "shop not found"
+            shop = ShopModel.query.filter_by(id=shop_id, user_id=user.id).first()
 
-        shop.title = data["title"]
-        db.session.commit()
+            if not shop:
+                return "shop not found"
 
-        return shop.to_dict()
+            if name:
+                shop.name = name
+
+            if address:
+                shop.address = address
+
+            if city:
+                shop.city = city
+
+            if latitude:
+                shop.latitude = latitude
+
+            if longitude:
+                shop.longitude = longitude
+
+            db.session.commit()
+
+            return shop.to_dict()
+
+        except Exception as e:
+            return "something went wrong", 500
 
     @shop_required()
     def delete(self, shop_id, user):
-        shop = ShopModel.query.filter_by(id=shop_id, user_id=user.id).first()
+        try:
+            shop = ShopModel.query.filter_by(id=shop_id, user_id=user.id).first()
 
-        if not shop:
-            return "shop not found", 404
+            if not shop:
+                return "shop not found", 404
 
-        db.session.delete(shop)
-        db.session.commit()
+            db.session.delete(shop)
+            db.session.commit()
 
-        return "ok", 200
+            return "ok", 200
+
+        except Exception as e:
+            return f"something went wrong: {e}", 500
